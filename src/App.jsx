e1f15a85220846b9dd6ea3151ed7254670ba1141
maria gabriela <gabrielaanselmao@gmail.com>
2024-03-05 19:48:28 -0300

    refactoring

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,10 @@
 import React from 'react';
 import PrecisoEstudar from './components/PrecisoEstudar';
-import { dobrarNumeros } from './utils/dobrarNumeros';
 
 function App() {
-  const listaNumeros = [1, 2, 3, 4, 5];
-  const numerosDobrados = dobrarNumeros(listaNumeros);
-
   return (
     <div>
       <PrecisoEstudar nomeDaTecnologia="React" />
-      <div>
-        Números dobrados: {numerosDobrados.join(', ')}
-      </div>
     </div>
   );
 }
