c12b32dffd57a0a30409450e5b3015e280dd9f01
maria gabriela <gabrielaanselmao@gmail.com>
2024-03-05 20:04:30 -0300

    refactoring

--- src/utils/dobrarNumeros.js
+++ src/utils/dobrarNumeros.js
@@ -1,4 +1,8 @@
-export function dobrarNumeros(numeros) {
-    return numeros.map(numero => numero * 2);
+function dobrarNumeros(numeros) {
+    const numerosDobrados = numeros.map(numero => numero * 2);
+    return numerosDobrados;
   }
   
+  const listaNumeros = [1, 2, 3, 4, 5];
+  const resultado = dobrarNumeros(listaNumeros);
+  console.log(resultado);
